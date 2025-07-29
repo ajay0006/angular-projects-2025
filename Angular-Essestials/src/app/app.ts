2e3b4919532410a9e3b81106379f46dec5edf5ae
damilola <ajay0006@algonquinlive.com>
2025-07-29 10:34:52 -0400

    added the task management logo and also included a css stylesheet for the header component

--- Angular-Essestials/src/app/app.ts
+++ Angular-Essestials/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//importing the header component
+import { Header } from "../header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+// using the imported header component as an import, that way the app component can use it aas its child
+// component
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

f3e4799b2d884e1baeb28c12df244e8b3bf9a038
damilola <ajay0006@algonquinlive.com>
2025-09-24 15:03:25 -0400

    120 - Dynamic inline style binding

--- Angular-Dashboard/src/shared/formcontrol/formcontrol.ts
+++ Angular-Dashboard/src/shared/formcontrol/formcontrol.ts
@@ -5,12 +5,21 @@ import { Component, Host, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './formcontrol.html',
   styleUrl: './formcontrol.css',
+  /* this disables the style being scoped it its component, that means that the forControl.css will 
+   be scoped to the global host 
+   */
   encapsulation: ViewEncapsulation.None,
-  // this will add the values specified here as pptys on the host element
-  // in this case i am adding the style class 'control', which is equivalent to me
-  // going into the html file and adding the class 'control' to the new-tickethost element
-  host: { class: 'control' }
+  /* this will add the values specified here as pptys on the host element
+  in this case i am adding the style class 'control', which is equivalent to me
+  going into the html file and adding the class 'control' to the new-tickethost element 
+  */
+  host: {
+    class: 'control',
+    'click': 'onClick()',
+  }
 })
 export class Formcontrol {
-
+  onClick() {
+    console.log('clicked');
+  }
 }

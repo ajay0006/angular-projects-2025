0c8f4c679a792b364ac68754303f49b31793282c
damilola <ajay0006@algonquinlive.com>
2025-09-24 20:32:39 -0400

    131 uing viewchild signal function

--- Angular-Dashboard/src/dashboard/support-tickets/new-ticket/new-ticket.ts
+++ Angular-Dashboard/src/dashboard/support-tickets/new-ticket/new-ticket.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { Button } from "../../../shared/button/button";
 import { Formcontrol } from "../../../shared/formcontrol/formcontrol";
 import { FormsModule } from '@angular/forms';
@@ -10,9 +10,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.css'
 })
 export class NewTicket {
+  /* i am using ViewChild to get access to the child components for this template, in this case that is the form element
+  the form would be a type HtmlFormElement that is wrapped in an Element Ref
+  rather than passing the #form template variable to the onSubmit function, i can use this method to access the form element
+  */
+  @ViewChild('form') formOldWay?: ElementRef<HTMLFormElement>
 
-  onSubmit(title: string, description: string) {
+  // keep in mind that if you are using required, it means you are sure that there is a template variable named form
+  private formNewWay = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
-  }
+  onSubmitViewChild(title: string, description: string) {
+    console.log(title);
+    console.log(description);
+    this.formOldWay?.nativeElement.reset();
+    this.formNewWay().nativeElement.reset();
+}
+
+  onSubmitTemplateVariable(title: string, description: string, form: HTMLFormElement) {
+    console.log(title);
+    console.log(description);
+    form.reset();
+}
 
 }

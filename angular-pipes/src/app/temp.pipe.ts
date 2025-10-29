d96245efa7d16767989fe1bb36ffaff300ed3dfc
damilola <ajay0006@algonquinlive.com>
2025-10-29 08:47:24 -0400

    pipes

---
+++ angular-pipes/src/app/temp.pipe.ts
@@ -0,0 +1,48 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'temperature',
+    standalone: true
+})
+export class TemperaturePipe implements PipeTransform {
+    transform(value: string | number | null, inputType: 'cel' | 'fah', outputType?: 'cel' | 'fah') {
+
+        if (!value) {
+            return value;
+        }
+
+        let val: number;
+
+        if (typeof value === 'string') {
+            val = parseFloat(value);
+        } else {
+            val = value;
+        }
+
+        let outPutTemp: number;
+
+
+        if (inputType === 'cel' && outputType === 'fah') {
+            outPutTemp = val * (9 / 5) + 32;
+        }
+        else if (inputType === 'fah' && outputType === 'cel') {
+            outPutTemp = (val - 32) * (5 / 9);
+        }
+        else {
+            outPutTemp = val;
+        }
+
+        let symbol: '°F' | '°C';
+
+        if (!outputType) {
+            symbol = inputType === 'cel' ? '°C' : '°F';
+        }
+        else {
+            symbol = outputType === 'fah' ? '°F' : '°C';
+        }
+
+        return `${outPutTemp.toFixed(2)} ${symbol}`;
+
+    }
+
+}
